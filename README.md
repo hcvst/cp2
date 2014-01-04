35bf0f33669a1fe3095a66d52c1c37e284ab3951
HC <hc@vst.io>
2014-01-04 08:01:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+Companionplanting.org
---
+++ cp2.erl
@@ -0,0 +1,31 @@
+%% @author Hans Christian von Stockhausen
+%% @copyright 2014 Hans Christian von Stockhausen
+%% Generated on 2014-01-04
+%% @doc This site was based on the 'empty' skeleton.
+
+%% Licensed under the Apache License, Version 2.0 (the "License");
+%% you may not use this file except in compliance with the License.
+%% You may obtain a copy of the License at
+%% 
+%%     http://www.apache.org/licenses/LICENSE-2.0
+%% 
+%% Unless required by applicable law or agreed to in writing, software
+%% distributed under the License is distributed on an "AS IS" BASIS,
+%% WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%% See the License for the specific language governing permissions and
+%% limitations under the License.
+
+-module(cp2).
+-author("Hans Christian von Stockhausen").
+
+-mod_title("cp2 zotonic site").
+-mod_description("An empty Zotonic site, to base your site on.").
+-mod_prio(10).
+
+-include_lib("zotonic.hrl").
+
+
+%%====================================================================
+%% support functions go here
+%%====================================================================
+
